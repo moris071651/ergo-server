4f11accbda5416be8cf491566d2a4a8c5f08466d
moris071651 <moris071651@proton.me>
2025-10-04 20:09:02 +0300

    ADD: setup the orm part

---
+++ app/db/session.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql+asyncpg://user:pass@localhost/dbname"
+
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+
+async def get_db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+            
+        except:
+            await session.rollback()
+            raise    
