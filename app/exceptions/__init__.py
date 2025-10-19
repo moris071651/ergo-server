05567a3bd27814746de6515c60f483ef63c99eec
moris071651 <moris071651@proton.me>
2025-10-19 08:17:08 +0300

    UPDATE: log on exception

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -1,7 +1,12 @@
+import logging
 from typing import Optional
 from fastapi import Request, status
 from app.schemas.error import ErrorResponse
 from starlette.responses import JSONResponse
+
+
+logger = logging.getLogger(__name__)
+
 
 class CustomBaseException(Exception):
     def __init__(self, status: int, message: str, detail: Optional[str] = None):            
@@ -14,21 +19,24 @@ class CustomBaseException(Exception):
 def setup_error_handling(app):
     @app.exception_handler(CustomBaseException)
     def handle_server_errors(request: Request, exception: CustomBaseException) -> ErrorResponse:
+        if exception.detail:
+            logger.error(exception.detail)
+
         return JSONResponse(
             status_code=exception.status,
             content=ErrorResponse(
                 error=exception.error,
-                detail=exception.detail
             ).model_dump()
         )
     
     @app.exception_handler(Exception)
     def handle_generic_errors(request: Request, exception: Exception) -> ErrorResponse:
+        logger.error(str(exception))
+
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content=ErrorResponse(
-                error="Internal Server Error",
-                detail=str(exception)
+                error="Internal Server Error"
             ).model_dump()
         )
     
