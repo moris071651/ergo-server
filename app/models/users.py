f1d419d619b950b370495de551b853723b3dfd7b
moris071651 <moris071651@proton.me>
2025-10-27 16:06:35 +0200

    UPDATE: add some useful field in the db

--- app/models/users.py
+++ app/models/users.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import UUID, Column, String
+from sqlalchemy import UUID, Column, DateTime, String, func
 from app.db.session import Base
 
 
@@ -7,6 +7,14 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    username = Column(String, index=True)
+
+    email = Column(String, index=True)
+    profile_image_key = Column(String, nullable=True)
     hashed_password = Column(String)
+
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
