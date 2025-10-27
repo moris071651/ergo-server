61baf3ff9dab90c006537b70230aa8d9a3fa5a28
moris071651 <moris071651@proton.me>
2025-10-27 16:04:39 +0200

    ADD: user router

--- app/routers/v1/__init__.py
+++ app/routers/v1/__init__.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from app.routers.v1.auth import router as auth
+from app.routers.v1.users import router as users
 
 router = APIRouter(prefix='/v1')
+
 router.include_router(auth)
+router.include_router(users)
