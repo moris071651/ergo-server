2d1b0ae88db5d8ffac868fb16cde7bebde41de18
moris071651 <moris071651@proton.me>
2025-10-27 16:08:27 +0200

    ADD: user schemas

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,3 +1,27 @@
-import uuid
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
 from pydantic import BaseModel, EmailStr
 
+
+class UserResponse(BaseModel):
+    id: UUID
+    email: EmailStr
+    profile_image_url: Optional[str]
+    first_name: str
+    last_name: str
+
+
+class CurrentUserResponse(UserResponse):
+    created_at: datetime
+    updated_at: datetime
+
+
+class UpdateUserRequest(BaseModel):
+    email: Optional[EmailStr] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+
+
+class UserPictureResponse(BaseModel):
+    profile_image_url: Optional[str]
