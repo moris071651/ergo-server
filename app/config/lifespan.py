4cddaba1ed3ba4efd6fc0af2ab45ddcaeb1a3c93
moris071651 <moris071651@proton.me>
2025-10-29 17:19:36 +0200

    ADD: create the buckets automatically

--- app/config/lifespan.py
+++ app/config/lifespan.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 import logging
 
 from app.config.redis import close_redis, init_redis
+from app.config.settings import BUCKET_USER_PICTURE
 from app.db.session import init_models
+from app.utils.storage import get_storage_adapter
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +13,10 @@ logger = logging.getLogger(__name__)
 async def lifespan(app: FastAPI):
     await init_redis()
     await init_models()
+
+    if not get_storage_adapter().create_bucket(BUCKET_USER_PICTURE):
+        logger.info(f"Creation of '{BUCKET_USER_PICTURE}' bucket skipped, already exists")
+
     logger.info("FastAPI started")
     yield
     await close_redis()
