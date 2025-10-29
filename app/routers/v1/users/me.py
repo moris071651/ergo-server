26067ddeb04b0211f4f642cfd6d1e7726e9e32b4
moris071651 <moris071651@proton.me>
2025-10-29 18:10:32 +0200

    UPDATE: me is me

---
+++ app/routers/v1/users/me.py
@@ -0,0 +1,107 @@
+from fastapi import APIRouter, Depends, File, Request, Response, UploadFile, status
+from app.config.settings import AUTH_COOKIE_KEY
+from app.db.session import Session, get_db
+
+from app.exceptions.auth import UserNotLoggedInException
+from app.schemas.users import CurrentUserResponse, UpdateUserRequest, UserPictureResponse
+from app.services.users import me as service
+from app.utils.storage import get_storage_adapter
+from app.utils.storage.base import StorageAdapter
+from app.utils.token import revoke_token
+
+
+router = APIRouter(prefix='/users/me', tags=['Current user'])
+
+
+@router.get("/")
+async def get_current_user(
+    req: Request,
+    db: Session = Depends(get_db)
+) -> CurrentUserResponse:
+    if req.state.user_id is None:
+        raise UserNotLoggedInException()
+
+    user_id = req.state.user_id
+    return await service.get_current_user(user_id, db)
+
+
+@router.patch('/')
+async def update_current_user(
+    req: Request,
+    update: UpdateUserRequest,
+    db: Session = Depends(get_db)
+) -> CurrentUserResponse:
+    if req.state.user_id is None:
+        raise UserNotLoggedInException()
+
+    user_id = req.state.user_id
+    return await service.update_current_user(user_id, update, db)
+
+
+@router.delete('/')
+async def deactivate_current_user(
+    req: Request,
+    res: Response,
+    db: Session = Depends(get_db)
+):
+    if req.state.user is None:
+        raise UserNotLoggedInException()
+    
+    user_id = req.state.user_id
+    await service.deactivate_current_user(user_id, db)
+
+    await revoke_token(
+        jti = req.state.jwt['jti'],
+        exp_timestamp = req.state.jwt['exp']
+    )
+
+    res.status_code = status.HTTP_204_NO_CONTENT
+    res.delete_cookie(AUTH_COOKIE_KEY)
+
+
+@router.get('/picture')
+async def get_current_user_picture(
+    req: Request,
+    db: Session = Depends(get_db),
+    storage: StorageAdapter = Depends(get_storage_adapter)
+) -> UserPictureResponse:
+    if req.state.user_id is None:
+        raise UserNotLoggedInException()
+
+    user_id = req.state.user_id
+    return await service.get_current_user_picture(user_id, db, storage)
+
+
+@router.put('/picture')
+async def update_current_user_picture(
+    req: Request,
+    file: UploadFile = File(...),
+    storage: StorageAdapter = Depends(get_storage_adapter),
+    db: Session = Depends(get_db)
+) -> UserPictureResponse:
+    if req.state.user_id is None:
+        raise UserNotLoggedInException()
+
+    user_id = req.state.user_id
+    return await service.update_current_user_picture(
+        file_bytes = await file.read(),
+        file_name = file.filename,
+        storage = storage,
+        user_id = user_id,
+        db = db
+    )
+
+
+@router.delete('/picture')
+async def delete_current_user_picture(
+    req: Request,
+    res: Response,
+    db: Session = Depends(get_db),
+    storage: StorageAdapter = Depends(get_storage_adapter)
+):
+    if req.state.user_id is None:
+        raise UserNotLoggedInException()
+
+    user_id = req.state.user_id
+    await service.delete_current_user_picture(user_id, db, storage)
+    res.status_code = status.HTTP_204_NO_CONTENT
