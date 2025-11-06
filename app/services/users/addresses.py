58ea4227bba4f2a465cfc195700c4a10c3e5d13f
moris071651 <moris071651@proton.me>
2025-11-06 17:08:04 +0200

    ADD: get_addresses_by_id

--- app/services/users/addresses.py
+++ app/services/users/addresses.py
@@ -10,3 +10,16 @@ async def get_all_addresses(db: AsyncSession, user_id: UUID) -> List[AddressResp
     result = await db.execute(select(Address).where(Address.user_id == user_id))
     addresses = result.scalars().all()
     return [AddressResponse.model_validate(addr) for addr in addresses]
+
+
+async def get_addresses_by_id(db: AsyncSession, user_id: UUID, address_id: UUID) -> AddressResponse:
+    stmt = select(Address).where(Address.id == address_id, Address.user_id == user_id)
+    result = await db.execute(stmt)
+    address = result.scalars().first()
+
+    if not address:
+        raise Exception()
+
+    return AddressResponse.model_validate(address)
+
+
