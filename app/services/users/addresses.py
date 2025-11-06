76c18475f2918cdf47abe915c41c18b19f4556f6
moris071651 <moris071651@proton.me>
2025-11-06 17:09:26 +0200

    ADD: remove_address service

--- app/services/users/addresses.py
+++ app/services/users/addresses.py
@@ -23,3 +23,13 @@ async def get_addresses_by_id(db: AsyncSession, user_id: UUID, address_id: UUID)
     return AddressResponse.model_validate(address)
 
 
+async def remove_address(db: AsyncSession, user_id: UUID, address_id: UUID) -> None:
+    stmt = select(Address).where(Address.id == address_id, Address.user_id == user_id)
+    result = await db.execute(stmt)
+    address = result.scalars().first()
+
+    if not address:
+        raise Exception()
+    
+    await db.delete(address)
+    await db.commit()
