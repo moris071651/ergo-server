525d4caa3a0d8c69ccf5502b3e69e47daf74b1bb
moris071651 <moris071651@proton.me>
2025-11-06 16:21:09 +0200

    ADD: get all addresses route

--- app/routers/v1/users/address.py
+++ app/routers/v1/users/address.py
@@ -1,30 +1,24 @@
 
-from fastapi import APIRouter
+from typing import List
+from uuid import UUID
+from fastapi import APIRouter, Depends, Request
+from app.db.session import Session, get_db
+from app.exceptions.auth import UserNotLoggedInException
+from app.services.users import address as service
+from app.schemas.address import AddressResponse
 
 
 router = APIRouter(prefix='/users/me/address', tags=["Current user's addresses"])
 
 
 @router.get('/')
-async def get_all_addresses():
-    pass
+async def get_all_addresses(
+    req: Request,
+    db: Session = Depends(get_db),
+) -> List[AddressResponse]:
+    if req.state.user_id is None:
+        raise UserNotLoggedInException()
 
+    user_id = req.state.user_id
+    return await service.get_all_addresses(db, user_id)
 
-@router.get('/{address_id}')
-async def get_addresses_by_id():
-    pass
-
-
-@router.post()
-async def add_address():
-    pass
-
-
-@router.patch()
-async def update_address():
-    pass
-
-
-@router.delete()
-async def remove_address():
-    pass
