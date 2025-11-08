26bcd7a569d5cbe1dff3bed47eae809183fb356b
moris071651 <moris071651@proton.me>
2025-11-08 21:05:36 +0200

    ADD: router template ready

--- app/routers/v1/workers/me.py
+++ app/routers/v1/workers/me.py
@@ -8,14 +8,16 @@ router = APIRouter(prefix='/me')
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-async def register_worker(
+async def activate_worker(
     req: Request,
+    worker_data: WorkerCreate,
     db: Session = Depends(get_db)
 ) -> CurrentWorkerResponse:
     if req.state.user_id is None:
         raise UserNotLoggedInException()
 
-    pass
+    user_id = req.state.user_id
+    return await service.activate_worker(db, user_id, worker_data)
 
 
 @router.get('/')
@@ -26,26 +28,30 @@ async def get_my_worker_profile(
     if req.state.user_id is None:
         raise UserNotLoggedInException()
 
-    pass
+    user_id = req.state.user_id
+    return await service.get_my_worker_profile(db, user_id)
 
 
 @router.patch('/')
 async def update_worker_profile(
     req: Request,
+    worker_data: WorkerUpdate,
     db: Session = Depends(get_db)
 ) -> CurrentWorkerResponse:
     if req.state.user_id is None:
         raise UserNotLoggedInException()
 
-    pass
+    user_id = req.state.user_id
+    return await service.update_worker_profile(db, user_id, worker_data)
 
 
 @router.delete('/', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_worker_profile(
+async def deactivate_worker_profile(
     req: Request,
     db: Session = Depends(get_db)
 ):
     if req.state.user_id is None:
         raise UserNotLoggedInException()
 
-    pass
+    user_id = req.state.user_id
+    await service.deactivate_worker_profile(db, user_id)
