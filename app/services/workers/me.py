b7d26b879d5e633b25bc3c99028a61f973749780
moris071651 <moris071651@proton.me>
2025-11-22 07:30:49 +0200

    ADD: create_worker service

---
+++ app/services/workers/me.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+from app.db.session import Session
+from app.models.workers import Worker
+from app.schemas.workers import CurrentWorkerResponse, WorkerCreate, WorkerUpdate
+from app.services.workers.skills import add_skills
+from app.utils.workers import get_worker, get_worker_panic
+
+
+async def create_worker(
+    db: Session,
+    user_id: UUID,
+    data: Optional[WorkerCreate]
+) -> CurrentWorkerResponse:
+    worker = await get_worker(db, user_id, True)
+    if worker is not None:
+        if worker.deleted_at is not None:
+            worker.deleted_at = None
+            await db.commit()
+            await db.refresh(worker)
+            return CurrentWorkerResponse.model_validate(worker)
+        
+        else:
+            raise Exception()
+        
+    if data is None:
+        raise Exception()
+
+    new_worker = Worker(
+        user_id=user_id,
+        bio=data.bio,
+        service_radius_km=data.service_radius_km,
+        experience_years=data.experience_years,
+    )
+
+    db.add(new_worker)
+    await db.commit()
+    await db.refresh(new_worker)
+
+    if data.skills:
+        await add_skills(db, user_id, data.skills, new_worker)
+        await db.refresh(new_worker)
+
+    return CurrentWorkerResponse.model_validate(new_worker)
