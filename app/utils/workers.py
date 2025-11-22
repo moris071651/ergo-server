5f5b5385bc32d94a1906fef6fe3e44e30d58ccb3
moris071651 <moris071651@proton.me>
2025-11-22 07:33:05 +0200

    UPDATE: include_deleted param

--- app/utils/workers.py
+++ app/utils/workers.py
@@ -6,15 +6,22 @@ from app.db.session import Session
 from app.models.workers import Worker
 
 
-async def get_worker(db: Session, user_id: UUID) -> Optional[Worker]:
-    stmt = select(Worker).where(Worker.user_id == user_id)
+async def get_worker(db: Session, user_id: UUID, include_deleted: bool = False) -> Optional[Worker]:
+    stmt = select(Worker).where(Worker.user_id == user_id and Worker.deleted_at == None)
+    
+    if include_deleted == True:
+        stmt = stmt.where(Worker.user_id == user_id)
+
+    else:
+        stmt = stmt.where(Worker.user_id == user_id and Worker.deleted_at == None)
+
     result = await db.execute(stmt)
     worker = result.scalars().first()    
     return worker
 
 
-async def get_worker_panic(db: Session, user_id: UUID) -> Worker:
-    worker = await get_worker(db, user_id)
+async def get_worker_panic(db: Session, user_id: UUID, include_deleted: bool = False) -> Worker:
+    worker = await get_worker(db, user_id, include_deleted)
 
     if not worker:
         raise Exception("Worker not found")
